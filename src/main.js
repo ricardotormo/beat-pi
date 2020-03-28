3385390d99df2d46faed02ac0614966dc3df2b13
Ricardo Tormo <ricardotormo.es@gmail.com>
2020-03-28 14:25:35 +0100

    fix bug (remove unnecessary store)

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import Fragment from "vue-fragment";
 
-import { store } from "./store/store";
 Vue.use(Fragment.Plugin);
 Vue.config.productionTip = false;
 
 new Vue({
   el: "#app",
-  store: store,
   render: h => h(App)
 }).$mount("#app");
